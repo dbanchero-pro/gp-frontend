07fb5b52aadee975360a891f04873c37794f2f82
dbanchero <dbanchero@quanam.com>
2026-02-04 16:28:50 -0300

    .

--- src/app/features/pliegos/components/repositorio-archivos/consulta-repositorio-archivos/consulta-repositorio-archivos.component.ts
+++ src/app/features/pliegos/components/repositorio-archivos/consulta-repositorio-archivos/consulta-repositorio-archivos.component.ts
@@ -60,50 +60,10 @@ export class ConsultaRepositorioArchivosComponent
   }
 
   override ngOnInit(): void {
+
     super.ngOnInit();
-
-    this.tiposArchivo = this.documentoService.obtenerTiposArchivo();
-
-    const paramVolver = this.route.snapshot.queryParamMap.get('volver');
-
-    if (paramVolver === '1') {
-      this.buscarVolver();
-
-      const currentUrl = this.router.url.split('?')[0];
-      this.router.navigateByUrl(currentUrl, { replaceUrl: true });
-    } else {
-      this.nuevaConsulta();
-    }
-  }
-
-  private buscarVolver(): void {
-    const snap = this.snapshotGenericService.load<any>(
-      ConsultaRepositorioArchivosComponent.SNAPSHOT_KEY
-    );
-    if (snap) {
-      const filtro = snap.filtro;
-      if (filtro) {
-        this.form.patchValue({
-          organismo: {
-            idInciso: filtro.idInciso,
-            idUnidadEjecutora: filtro.idUnidadEjecutora
-          },
-          nombreDocumento: filtro.nombreDocumento || '',
-          tipoArchivo: filtro.tipoArchivo || ''
-        });
-      }
-
-      this.parametros.pagina = snap.pagina ?? 0;
-      this.parametros.tamanoPagina = snap.tamanoPagina ?? 10;
-      this.parametros.sort = snap.sort ?? this.columnaOrdenInicial;
-      this.parametros.order = snap.order ?? this.ordenInicial;
-
-      setTimeout(() => {
-        this.buscar();
-      }, 200);
-    } else {
-      this.nuevaConsulta();
-    }
+    this.tiposArchivo = this.documentoService.obtenerTiposArchivo();   
+    this.nuevaConsulta();
   }
 
   onFiltroOrganismo(filtro: IFiltroOrganismoDTO | null): void {
@@ -187,8 +147,6 @@ export class ConsultaRepositorioArchivosComponent
     this.snapshotGenericService.clear(
       ConsultaRepositorioArchivosComponent.SNAPSHOT_KEY
     );
-
-    this.actualizarFiltrosYBuscar();
   }
 
   abrirAgregarDocumento(): void {
