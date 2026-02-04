ee5a2cb4cb633ddaea0f476c59d4b625d9681ba5
dbanchero-pro <dbanchero@quanam.com>
2026-02-04 09:56:10 -0300

    Updated menu.service.ts

--- src/app/shared/services/common/menu.service.ts
+++ src/app/shared/services/common/menu.service.ts
@@ -421,7 +421,7 @@ export class MenuService {
                         'GC_GESTION_USU.MODIFICACION',
                         'GC_GESTION_USU.CONSULTA',
                     ],
-                    url: '/administracion/gestion-usuarios/consulta-usuario-conformidad',
+                    url: '/pliegos/repositorio-archivos',
                 },
                 {
                     nombre: 'Secciones',
