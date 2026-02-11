02abdee4406d44f40be78febb42199ec484c399b
dbanchero-pro <dbanchero@quanam.com>
2026-02-11 12:08:42 -0300

    Updated bandeja-entrada.service.ts

--- src/app/features/pliegos/models/filtro-bandeja-entrada.model.ts
+++ src/app/features/pliegos/models/filtro-bandeja-entrada.model.ts
@@ -1,12 +1,14 @@
 import { EstadoProcesoPliego } from '../enum/estado-proceso-pliego.enum';
 
-export interface FiltroBandejaEntrada {
-  incisoId?: number | null;
-  unidadEjecutoraId?: number | null;
-  unidadCompraId?: number | null;
-  numeroCompra?: number | null;
-  anioCompra?: number | null;
-  tipoCompraId?: string | null;
-  estado?: EstadoProcesoPliego | null;
-  soloPublicadosVigentes?: boolean | null;
+export class FiltroBandejaEntrada {
+  constructor(
+    public incisoId?: number,
+    public unidadEjecutoraId?: number,
+    public unidadCompraId?: number,
+    public numeroCompra?: number,
+    public anioCompra?: number,
+    public tipoCompraId?: string,
+    public estado?: EstadoProcesoPliego,
+    public soloPublicadosVigentes?: boolean
+  ) {}
 }
